b5727fa4f79e18cf599934e9e621c5a07be4348e
Raptor <karl.b.albrecht@gmail.com>
2026-06-14 20:42:01 +0200

    feat: add supabase auth functions in lib/supabase/auth.ts

---
+++ lib/supabase/auth.ts
@@ -0,0 +1,33 @@
+"use server"
+
+import { createClient } from "@/lib/supabase/server"
+
+export async function signUp(formData: FormData) {
+  const email = formData.get("email") as string
+  const password = formData.get("password") as string
+
+  const supabase = await createClient()
+
+  await supabase.auth.signUp({
+    email,
+    password,
+  })
+}
+
+export async function signIn(formData: FormData) {
+  const email = formData.get("email") as string
+  const password = formData.get("password") as string
+
+  const supabase = await createClient()
+
+  await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+}
+
+export async function signOut() {
+  const supabase = await createClient()
+
+  await supabase.auth.signOut()
+}
