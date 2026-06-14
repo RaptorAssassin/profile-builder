931ea9b884f6d18111e73c6c2a66a33fbc7c638d
Raptor <karl.b.albrecht@gmail.com>
2026-06-14 12:34:17 +0200

    initial commit

---
+++ app/[username]/page.tsx
@@ -0,0 +1,9 @@
+type UserPageProps = {
+  params: {
+    username: string
+  }
+}
+
+export default function UserPage({ params: { username } }: UserPageProps) {
+  // page for testing layout and rough design
+}
---
+++ lib/data.ts
@@ -0,0 +1,11 @@
+// Usernames that cannot be chosen to prevent conflicts with existing pages and routes
+export const RESERVED_USERNAMES = [
+  "dashboard",
+  "login",
+  "signup",
+  "auth",
+  "admin",
+  "test",
+  "example",
+  "profile",
+]
