f48f392603d126c63bc59fd82d1287bc64e8d07f
Raptor <karl.b.albrecht@gmail.com>
2026-07-11 19:36:45 +0200

    feat: add discord oauth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
 import { login, signUp } from "@/lib/auth"
-import { OAuthGithub } from "@/lib/oauth"
+import { OAuthDiscord, OAuthGithub } from "@/lib/oauth"
 import { useRouter } from "next/navigation"
 import { Input } from "@/components/ui/input"
 import Image from "next/image"
@@ -27,9 +27,11 @@ export default function AuthPage() {
   return (
     <div className="flex h-dvh w-full items-center justify-center bg-background">
       <div className="mx-auto mt-20 flex w-full max-w-md flex-col items-start justify-center gap-4 rounded-(--radius) bg-accent/20 p-8">
+        <h1 className="w-full text-2xl font-extrabold">Login To Profile Builder</h1>
         <h1 className="">OAuth</h1>
         <div className="flex flex-wrap gap-2">
           <OAuthButton provider="github" />
+          <OAuthButton provider="discord" />
         </div>
         <h1 className="">Email</h1>
         <Input
@@ -62,14 +64,31 @@ export default function AuthPage() {
 }
 
 const providers = {
-  github: { name: "GitHub", iconSrc: "/icons/github.svg", function: OAuthGithub },
+  github: {
+    name: "GitHub",
+    iconSrc: "/icons/github.svg",
+    iconClass: "text-white",
+    function: OAuthGithub,
+  },
+  discord: {
+    name: "Discord",
+    iconSrc: "/icons/discord.svg",
+    iconClass: "text-[#5865F2]",
+    function: OAuthDiscord,
+  },
 }
 
-function OAuthButton({ provider }: { provider: "github" }) {
+function OAuthButton({ provider }: { provider: keyof typeof providers }) {
   const { name, iconSrc, function: oauthFunction } = providers[provider]
   return (
     <Button onClick={oauthFunction} variant="outline" className="flex h-15 w-15 items-center p-2">
-      <Image src={iconSrc} alt={name} width={40} height={40} className="select-none" />
+      <Image
+        src={iconSrc}
+        alt={name}
+        width={40}
+        height={40}
+        className={`select-none`}
+      />
     </Button>
   )
 }
--- lib/oauth.ts
+++ lib/oauth.ts
@@ -3,11 +3,25 @@
 import { createClient } from "./supabase/client"
 
 export const OAuthGithub = async (): Promise<void> => {
-  console.log("OAuthGithub called with siteUrl:", window.location.origin)
   const supabase = await createClient()
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
+    options: {
+      redirectTo: `${window.location.origin}/auth/callback`,
+    },
+  })
+
+  if (error) {
+    console.error(error)
+  }
+}
+
+export const OAuthDiscord = async (): Promise<void> => {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "discord",
     options: {
       redirectTo: `${window.location.origin}/auth/callback`,
     },
